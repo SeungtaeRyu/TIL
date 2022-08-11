10f421409008077456370f8d7a3176e3d821857b
SeungtaeRyu <dbtmdxo1992@naver.com>
2022-08-11 17:20:20 +0900

    알고리즘 4일차

--- Algorithm/SWEA/4837.py
+++ Algorithm/SWEA/4837.py
@@ -1,20 +1,17 @@
-# import sys
-# sys.stdin = open("input4837.txt")
-#
-# T = int(input())
-# for tc in range(1, T+1):
-#     n, k = map(int, input().split())
-#
-#     numbers = []
-#     answer = []
-#     for i in range(1, n+1):
-#         numbers.append(i)
-#
-#     for i in range(1, 1<<n):
-#         total = 0
-#         for j in range(n):
-#             if i & (1 << j):
-#                 total += numbers[j]
-#         if total == k:
-#             answer.append('+')
-#     print(f'#{tc} {len(answer)}')
+T = int(input())
+for tc in range(1, T+1):
+    n, k = map(int, input().split())
+
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    answer = []
+
+    for i in range(1, 1 << 12):
+        total = 0                           # 합계 변수
+        count = 0                           # 더한 원소 횟수 변수
+        for j in range(12):
+            if i & (1 << j):
+                count += 1                  # 더한 원소 갯수 ++
+                total += numbers[j]         # 합계 ++
+        if total == k and count == n:       # 합계가 k 이면서 더한 원소가 n개 이면 결과 리스트에 +를 추가
+            answer.append('+')
+    print(f'#{tc} {len(answer)}')           # 결과 출력으로 +의 갯수를 출력한다.
